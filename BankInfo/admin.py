afbec5c1f1f17c59c99115057d4707b02bfb88f3
himanshu <hp746123@gmail.com>
2018-04-26 10:36:48 +0530

    Modifies files to add data to database

--- BankInfo/admin.py
+++ BankInfo/admin.py
@@ -1,3 +1,6 @@
 from django.contrib import admin
+from .models import Bank, Branch
 
 # Register your models here.
+admin.site.register (Bank)
+admin.site.register (Branch)
--- BankInfo/views.py
+++ BankInfo/views.py
@@ -5,6 +5,7 @@ from rest_framework import status
 from rest_framework.parsers import JSONParser
 from .models import Bank, Branch
 from .serializers import BranchSerializer
+import itertools
 
 # Create your views here.
 
@@ -73,28 +74,42 @@ def add_banks_from_file (request):
     return render (request, 'BankInfo/index.html')
 
 def add_branches_from_file (request):
-    print ("Add branches from file")
+	print ("Add branches from file")
     # bankinfo_branch (ifsc, bank_id, branch, address, city, district, state) FROM stdin;
-    filename = 'branches.txt'
-    fh = open (filename, "r")
-    count = 0
-    print ('================ starting adding branches to database =======================')
-    for line in fh:
-        words = line.split ('\t')
-        if (len (words)) >= 7:
-            try:
-                ifsc_code = words[0]
-                bank_id = int (words[1])
-                branch_name = words[2]
-                address = words[3]
-                city = words[4]
-                district = words[5]
-                state = words[6]
-                b = Branch (ifsc_code, bank_id, branch_name, address, city, district, state)
-                b.save ()
-                count += 1
-                print ('Number of branches added:', count)
-            except Exception as e:
-                print (str (e))
-    print ('=================', count, 'branches added to database ========================')
-    return render (request, 'BankInfo/index.html')
+	count = 0
+	lines_to_skip = 0
+
+	with open ('num_lines_to_skip.txt', 'r') as f:
+		for line in f:
+			lines_to_skip = int (line)
+			break
+    
+	filename = 'branches.txt'
+	fh = open (filename, "r")
+    
+	print ('================ starting adding branches to database =======================')
+	for line in itertools.islice (fh, lines_to_skip, None):
+		words = line.split ('\t')
+		if (len (words)) >= 7:
+			try:
+				ifsc_code = words[0]
+				bank_id = int (words[1])
+				branch_name = words[2]
+				address = words[3]
+				city = words[4]
+				district = words[5]
+				state = words[6]
+				b = Branch (ifsc_code, bank_id, branch_name, address, city, district, state)
+				b.save ()
+				count += 1
+				print ('Number of branches added:', count)
+				if count >= 500:
+					break
+			except Exception as e:
+				print (str (e))
+                
+	with open ('num_lines_to_skip.txt', 'w') as f:
+		f.write (str (lines_to_skip + count))
+                
+	print ('=================', count, 'branches added to database ========================')
+	return render (request, 'BankInfo/index.html')
